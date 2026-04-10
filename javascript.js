29782d7fe9c5273fbd2bd923d2cd7e8f6e6b50f5
Rivith Panthila <panthilarivith470@gmail.com>
2026-04-10 10:34:14 +0530

    experince slide started

--- javascript.js
+++ javascript.js
@@ -27,3 +27,28 @@ function typeEffect() {
 }
 
 typeEffect();
+
+
+// experience tab
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].classList.remove("active-content");
+    }
+
+    
+    tablinks = document.getElementsByClassName("tab-btn");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+
+    
+    document.getElementById(tabName).classList.add("active-content");
+    evt.currentTarget.classList.add("active");
+
+    
+}
